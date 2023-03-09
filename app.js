fd12b777db0092e3988c51f2563048186fcc4e96
YuZhangzhang <yzz0915@gmail.com>
2023-03-09 17:45:28 +0800

    use bcrypt to encrypt password

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -43,24 +44,34 @@ app.get("/register", function(req, res){
 
 
 app.post("/register",function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password) 
-    });
 
-    newUser.save()
-        .then(() => res.render("secrets"))
-        .catch(err => console.log(err)) 
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash 
+        });
+    
+        newUser.save()
+            .then(() => res.render("secrets"))
+            .catch(err => console.log(err)) 
+    })
+
+    
 })
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username })
         .then((foundUser) => {
-            if (foundUser.password === password) {
-                res.render("secrets")
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function(err, result){
+                    if (result === true) {
+                        res.render("secrets")
+                    }
+                })
+                
             } else {
             console.log('Document not found');
             }
