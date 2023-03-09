13f92fbd35d0346b6076235bd4a9b03ef5dd5414
YuZhangzhang <yzz0915@gmail.com>
2023-03-09 15:24:00 +0800

    md5 encryption

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 const app = express();
 
@@ -21,10 +21,9 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String 
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -46,7 +45,7 @@ app.get("/register", function(req, res){
 app.post("/register",function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password) 
     });
 
     newUser.save()
@@ -56,7 +55,7 @@ app.post("/register",function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username })
         .then((foundUser) => {
